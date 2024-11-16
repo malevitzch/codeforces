36d573245980e7ade1c5041bce9de99c6229be07
malevitzch <malevitzch@gmail.com>
2024-11-16 10:58:15 +0100

    Added sort for c++

---
+++ c++/sort.sh
@@ -0,0 +1,7 @@
+for file in *.rs; do
+  target_dir=$(echo "$file" | sed -n 's/^\([0-9]\+\).*$/\1/p')
+  if [ -n "$target_dir" ]; then
+    mkdir -p "$target_dir"
+    mv "$file" "$target_dir/"
+  fi
+done
