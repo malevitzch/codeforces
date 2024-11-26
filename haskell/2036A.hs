1c261976656e6099becffaece8a48b9d644bd995
malevitzch <malevitzch@gmail.com>
2024-11-26 20:43:48 +0100

    Added 2036A solution in haskell

--- haskell/2036A.hs
+++ haskell/2036A.hs
@@ -1,1 +1,21 @@
-main :: IO () = putStr "XD"
+solveCases :: Int -> IO ()
+solveCase :: [Int] -> Bool
+solveCase [x] = True
+solveCase (x : y : xs) =
+  let diff = abs (x - y)
+   in (diff == 5 || diff == 7) && solveCase (y : xs)
+solveCases 0 = return ()
+solveCases x = do
+  n <- (readLn :: IO Int)
+  input <- getLine
+  let arr = map read (words input) :: [Int]
+  let result = solveCase arr
+  if result
+    then putStrLn "YES"
+    else putStrLn "NO"
+  solveCases (x - 1)
+
+main :: IO ()
+main = do
+  t <- (readLn :: IO Int)
+  solveCases t
