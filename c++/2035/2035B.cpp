9c9135c882d4618c85c22311486f5048f883e84e
malevitzch <malevitzch@gmail.com>
2025-04-10 10:48:21 +0200

    Added solve for 2093A.cpp

---
+++ c++/2035/2035B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define DEBUG false
+
+#if DEBUG == 1
+#include "debug_lib.cpp"
+using namespace dbg;
+#endif // DEBUG
+using namespace std;
+
+
+void solve()
+{
+
+}
+
+int main()
+{
+  if(!DEBUG)
+  {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+  }
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    
+  }
+}
+/*
+
+*/
---
+++ c++/2093/2093A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define DEBUG false
+
+#if DEBUG == 1
+#include "debug_lib.cpp"
+using namespace dbg;
+#endif // DEBUG
+using namespace std;
+
+
+void solve()
+{
+  int k;
+  cin >> k;
+  cout << (k & 1 ? "YES" : "NO") << "\n";
+}
+
+int main()
+{
+  if(!DEBUG)
+  {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+  }
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    solve();
+  }
+}
+/*
+
+*/
