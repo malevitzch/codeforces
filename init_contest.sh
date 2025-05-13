4d791031ec537a521070d2b077943fb6bb827257
malevitzch <malevitzch@gmail.com>
2025-05-13 20:36:12 +0200

    Updated the init contest file to automatically get the date if no arguments are provided and to also generate a file to paste tests into

--- init_contest.sh
+++ init_contest.sh
@@ -1,32 +1,46 @@
-if [ "$#" -ne 3 ]; then
-  echo "wrong param number, found $# expected 3"
+if [ "$#" -eq 0 ]; then
+  year=$(date +%Y)
+  month=$(date +%m)
+  day=$(date +%d)
+elif [ "$#" -ne 3 ]; then
+  echo "Wrong number of parameters: found $#; expected 3 or 0."
   exit 1
+else
+  year=${1}
+  month=${2}
+  day=${3}
 fi
 declare -a arr=("A" "B" "C" "D" "E")
 
-if [ ! -d ${1} ]; then
-  mkdir ${1}
+
+if [ ! -d ${year} ]; then
+  mkdir ${year}
 fi
 
-if [ ! -d "${1}/${2}" ]; then
-  mkdir "${1}/${2}"
+if [ ! -d "${year}/${month}" ]; then
+  mkdir "${year}/${month}"
 fi
 
-if [ ! -d "${1}/${2}/${3}" ]; then
-  mkdir "${1}/${2}/${3}"
+if [ ! -d "${year}/${month}/${day}" ]; then
+  mkdir "${year}/${month}/${day}"
 fi
 
 
 FILENAME="debug_lib.cpp"
 if [ -f ${FILENAME} ] ; then
-  cp $FILENAME "${1}/${2}/${3}/${FILENAME}"
+  cp $FILENAME "${year}/${month}/${day}/${FILENAME}"
 fi
 
 for i in ${arr[@]}; do
-  FILENAME=${1}/${2}/${3}/Problem${i}.cpp
+  FILENAME=${year}/${month}/${day}/Problem${i}.cpp
   if [ -f ${FILENAME} ] ; then
     echo "Error: ${FILENAME} already exists. Skipping"
   else
     cp "Template.cpp" ${FILENAME}
   fi
 done
+
+FILENAME="${year}/${month}/${day}/test"
+if [ ! -f ${FILENAME} ]; then
+  touch ${FILENAME}
+fi
