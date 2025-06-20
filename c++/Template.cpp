596e2bd4fe52c0aa3d7527deeef25f312edaa19d
malevitzch <malevitzch@gmail.com>
2025-06-20 19:07:25 +0200

    Updated the template with more cursed stuff

--- c++/Template.cpp
+++ c++/Template.cpp
@@ -1,11 +1,22 @@
 #include <bits/stdc++.h>
+using namespace std;
+
 #define DEBUG false
+
+#define ll long long
+#define ull unsigned long long
+
+#define vi vector<int>
+#define vll vector<long long>
+#define vull vector<unsigned long long>
+#define vstr vector<string>
+
+#define bit(x,i) (x&(1<<i))
 
 #if DEBUG == 1
 #include "debug_lib.cpp"
 using namespace dbg;
 #endif // DEBUG
-using namespace std;
 
 
 void solve() {
