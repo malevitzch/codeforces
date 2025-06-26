3c3d5694b7fb46f6898bad367fcaa818dbe02b9a
malevitzch <malevitzch@gmail.com>
2025-06-26 19:53:53 +0200

    Added a version of the Template that is better suited for testing

---
+++ c++/TestableTemplate.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define DEBUG false
+
+#define ll long long
+#define ull unsigned long long
+
+#define vi vector<int>
+#define vll vector<long long>
+#define vull vector<unsigned long long>
+#define vstr vector<string>
+
+#define bit(x,i) (x&(1<<i))
+
+#if DEBUG == 1
+#include "debug_lib.cpp"
+using namespace dbg;
+#endif // DEBUG
+
+struct TestData {
+
+};
+
+void solve(TestData data) {
+
+}
+
+int main() {
+  if(!DEBUG) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+  }
+  int t;
+  cin>>t;
+  while(t--) {
+    TestData data;
+
+
+
+    solve(data);
+  }
+}
+/*
+
+*/
