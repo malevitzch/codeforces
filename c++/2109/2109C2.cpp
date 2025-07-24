f65b3933994b19f9fe5b9edfd5f463265a44b4b2
malevitzch <malevitzch@gmail.com>
2025-07-24 09:52:33 +0200

    Added a solution for 2109C2 (Really cool problem, found out the divisibility by 9 by bruteforcing sequences of operations for smaller ranges of x)

---
+++ c++/2109/2109C2.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <ostream>
+using namespace std;
+
+#define DEBUG false
+
+#define ll long long
+#define ull unsigned long long
+
+#define ipair pair<int, int>
+#define llpair pair<ll, ll>
+
+#define vi vector<int>
+#define vll vector<long long>
+#define vull vector<unsigned long long>
+#define vstr vector<string>
+
+#define bit(x,i) (x&(1<<i))
+
+#if DEBUG == 1
+#include "debug_lib.cpp"
+using namespace dbg;
+#endif // DEBUG
+
+void YES() {cout << "YES\n";}
+void NO() {cout << "NO\n";}
+void answer(bool b) {b ? YES() : NO();}
+ll inp() {ll x; cin >> x; return x;}
+ll& inp(ll& x) {cin >> x; return x;}
+int& inp(int& x) {cin >> x; return x;}
+
+void findX(ll n) {
+  cout << "mul " << 9 << "\n";
+  cout.flush();
+  int ok;
+  cin >> ok;
+  for(int i = 0; i < 2; i++) {
+    cout << "digit\n";
+    cout.flush();
+    int x;
+    cin >> x;
+  }
+  cout << "add " << (n - 9) << "\n";
+  cout.flush();
+  cin >> ok;
+}
+
+void solve() {
+  ll n = inp();
+  findX(n);
+  cout << "!\n";
+  cout.flush();
+  int ok;
+  cin >> ok;
+}
+
+int main() {
+  int t;
+  cin>>t;
+  while(t--) {
+    solve();
+  }
+}
+/*
+
+*/
