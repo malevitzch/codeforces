186e982e599f6e677b203b69da5a1208594c209a
malevitzch <malevitzch@gmail.com>
2025-09-13 13:53:05 +0200

    Added a basic graphs library file with toposort and SCC

---
+++ c++/lib/graphs_basic.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct graph_t {
+  int n;
+  vector<vector<int>> g;
+
+  vector<int>& operator[](int i) { return g[i]; }
+  graph_t(int size) : n(size), g(size+1) {}
+  graph_t() = default;
+  vector<int> nodes() {
+    vector<int> v(n);
+    iota(v.begin(), v.end(), 1);
+    return v;
+  }
+};
+
+void topo_dfs(int node, graph_t& graph, vector<bool>& vis, vector<int>& res) {
+  vis[node] = true;
+  for(int x : graph[node]) if(!vis[x]) topo_dfs(x, graph, vis, res);
+  res.push_back(node);
+}
+
+// Assumes nodes are ordered from 1 to n
+vector<int> toposort(graph_t& graph) {
+  vector<int> result;
+  vector<bool> vis(graph.n+1);
+  for(int i : graph.nodes()) if(!vis[i]) topo_dfs(i, graph, vis, result);
+  reverse(result.begin(), result.end());
+  return result;
+}
+
+graph_t reverse_graph(graph_t& graph) {
+  graph_t rev_graph(graph.n+1);
+  for(int i : graph.nodes()) for(int x : graph[i]) rev_graph[x].push_back(i);
+  return rev_graph;
+}
+
+struct scc_t {
+  int n;
+  vector<int> which;
+  graph_t g;
+
+  scc_t(int size) : n(size), which(n+1) {}
+  int& operator[](int i) { return which[i]; }
+};
+
+void paint(int node, int color, graph_t& graph, vector<int>& colors) {
+  colors[node] = color;
+  for(int x : graph[node]) if(!colors[x]) paint(x, color, graph, colors);
+}
+
+scc_t scc(graph_t& graph, int n) {
+  vector<int> order = toposort(graph);
+  graph_t rg = reverse_graph(graph);
+  scc_t res(n);
+  int color = 0;
+  for(int v : order) if(!res[v]) paint(v, ++color, rg, res.which);
+  res.g = graph_t(color);
+  for(int i : graph.nodes()) for(int x : graph[i]) if(res[i] != res[x]) res.g[i].push_back(x);
+  return res;
+}
