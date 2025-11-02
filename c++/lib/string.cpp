e728dfb204cbe58e74f27548689251918a17b207
malevitzch <malevitzch@gmail.com>
2025-11-02 17:22:51 +0100

    Added a function to find all occurrences of a string inside another

--- c++/lib/string.cpp
+++ c++/lib/string.cpp
@@ -6,9 +6,19 @@ vector<int> prefix_func(string s) {
   vector<int> res(n);
   for(int i = 1; i < n; i++) {
     int j = res[i-1];
-    while(j > 0 && s[i] != s[i]) j = res[j-1];
+    while(j > 0 && s[i] != s[j]) j = res[j-1];
     if(s[i] == s[j]) j++;
     res[i] = j;
   }
   return res;
+}
+
+vector<int> find_all_occurences(string pattern, string str) {
+  string s = pattern + "$" + str;
+  auto pf = prefix_func(s);
+  vector<int> ans;
+  for(int i = 0; i < s.size(); i++) {
+    if(pf[i] == pattern.size()) ans.push_back(i - 2 * pattern.size());
+  }
+  return ans;
 }
